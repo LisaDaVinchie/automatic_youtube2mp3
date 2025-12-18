b697056aad9b24c6bbad4a7a78d290ff9e4dfe15
LisaDaVinchie <lisadavinchie@gmail.com>
2025-12-18 13:39:33 +0100

    Fixxed kivy bug

--- src/download.py
+++ src/download.py
@@ -18,18 +18,9 @@ class Downloader:
         self.url = url
         output_path = self._create_output_path()
         ydl_opts = self._download_options(output_path)
+        
         with YoutubeDL(ydl_opts) as ydl:
-            print("Starting download...")
-            try:
-                ydl.download([self.url])
-                print("Download completed.\n")
-            except Exception as e:
-                print("An error occurred during download:", str(e))
-                return
-
-    def _remove_playlist_component(self):
-        if "watch" in self.url and "&list=" in self.url:
-            self.url = self.url.split("&list=")[0]
+            ydl.download([self.url])
 
     def _create_output_path(self) -> str:
         file_template = ""
@@ -38,14 +29,17 @@ class Downloader:
                 
         if "playlist?list=" in self.url:
             file_template = "%(playlist)s/%(track_number)s - %(title)s.%(ext)s"
-            
-            # file_template = "%(title)s.%(ext)s"
         else:
             file_template = "%(title)s.%(ext)s"
             
         return str(self.output_dir / file_template)
+    
+    def _remove_playlist_component(self):
+        if "watch" in self.url and "&list=" in self.url:
+            self.url = self.url.split("&list=")[0]
 
-    def _download_options(self, output_path_template: str = None, preferred_quality: int = 192, preferred_format: str = "mp3") -> dict:
+
+    def _download_options(self, output_path_template: str, preferred_quality: int = 192, preferred_format: str = "mp3") -> dict:
         ydl_opts = {
             "format": "bestaudio/best",
             "postprocessors": [{
@@ -53,14 +47,9 @@ class Downloader:
                 "preferredcodec": preferred_format,
                 "preferredquality": str(preferred_quality),
             }],
-            "outtmpl": output_path_template
+            "outtmpl": output_path_template,
+            "quiet": True,
+            "no_warnings": True
         }
         
         return ydl_opts
-
-# if __name__ == "__main__":
-#     downloader = Downloader(output_dir=Path("./output/"))
-#     url = sys.argv[1] if len(sys.argv) > 1 else input("Enter URL: ")
-    
-#     # url = "https://music.youtube.com/watch?v=uZwFVZgAuFk&list=OLAK5uy_mBj-sSPwi2H0fOlNpXgre7v-r73lUd6Zs"
-#     downloader.run(url)
